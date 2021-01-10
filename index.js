a3d715fe6040cb836a86ac69029d9dfb1e291892
lchan20 <59620735+lchan20@users.noreply.github.com>
2021-01-09 19:42:58 -0800

    added instructions for nodemon

--- index.js
+++ index.js
@@ -9,3 +9,20 @@ const PORT = process.env.PORT || "8000";
 app.listen(PORT, function() {
     console.log("server is running on local host 8000");
 });
+
+/* to start node application: 
+type into terminal " npm i" or "npm install" - to download all dependencies
+
+to start the server:
+type into terminal "node index.js" or "npm start" or 
+"nodemon index.js" (nodemon restarts the server each time you save 
+    a file with changes...the other ones require you to stop and restart the server each time to see the changes)
+
+    (if nodemon does not work try typing "Set-ExecutionPolicy RemoteSigned -Scope CurrentUser" into terminal)
+
+to stop server press CTRL + C in the terminal
+
+to see server application:
+in a browser type in "localhost::8000" 
+
+ */
